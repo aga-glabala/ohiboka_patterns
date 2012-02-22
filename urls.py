4e988632644c6881322602eaac1e194c460c8ac4
Agnieszka Glabala <aga.glabala@gmail.com>
2012-02-22 09:19:10 +0100

    Logowanie, baza, modele, część szablonów #1

---
+++ urls.py
@@ -0,0 +1,35 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    
+    # Examples:
+    url(r'^$', 'ohiboka_patterns.bracelet.views.home', name='home'),
+    url(r'^login$', 'ohiboka_patterns.bracelet.views.login_user'),
+    url(r'^logout$', 'ohiboka_patterns.bracelet.views.logout_user'),
+    #url(r'^$', 'ohiboka_patterns.bracelet.views.home', name='home'),
+    url(r'^bracelet/(?P<bracelet_id>\d+)/$', 'ohiboka_patterns.bracelet.views.bracelet'),
+    url(r'^add$', 'ohiboka_patterns.bracelet.views.add'),
+    
+    #url(r'^login/$', 'ohiboka_patterns.bracelet.views.login_user'),
+    # url(r'^ohiboka_patterns/', include('ohiboka_patterns.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += staticfiles_urlpatterns()
+
+#urlpatterns = patterns('',
+#    (r'^polls/$', 'mysite.polls.views.index'),
+#    (r'^polls/(?P<poll_id>\d+)/$', 'mysite.polls.views.detail'),
+#    (r'^polls/(?P<poll_id>\d+)/results/$', 'mysite.polls.views.results'),
+#    (r'^polls/(?P<poll_id>\d+)/vote/$', 'mysite.polls.views.vote'),
+#)
