9c487002d51bd99508fa5402581724407e317851
agnis <aga.glabala@gmail.com>
2012-07-07 19:51:49 +0200

    settings

--- settings_dev.py
+++ settings_dev.py
@@ -16,11 +16,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle$
-        'NAME': projectaddress + 'opdb.db', # Or path to database file if using sqlite3.
-        'USER': 'ohiboka', # Not used with sqlite3.
-        'PASSWORD': 'ohiboka', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'
+        'NAME': 'm11560_patterns2', # Or path to database file if using sqlite3.
+        'USER': 'm11560_patterns2', # Not used with sqlite3.
+        'PASSWORD': '0h1b0k@.2', # Not used with sqlite3.
+        'HOST': 'mysql.mydevil.net', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '', # Set to empty string for default. Not used with sqlite3.
     }
 }
