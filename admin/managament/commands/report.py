8f708795fa26c6114bdb548630e92912cb15d9e5
Marek Swiecznik <swieczman@gmail.com>
2012-10-13 22:59:20 +0200

    Weekly report

--- admin/managament/commands/report.py
+++ admin/managament/commands/report.py
@@ -12,12 +12,12 @@ class Command(NoArgsCommand):
 		nowe_bracelety = Bracelet.objects.filter(accepted = 0)
 		subject = '[Ohiboka Patterns] Raport z ' + str(datetime.date.today())
 		msg_content = u'Raport z '+str(datetime.date.today())+\
-			u'\r\n\r\nW ciągu ostatniego tygodnia pojawiło się '+str(len(nowe_bracelety))+u' nowych wzorów.'+\
+			u'\r\n\r\W serwisie jest '+str(len(nowe_bracelety))+u' wzorów do zweryfikowania.'+\
 			u'Przejrzyj je tutaj: http://patterns.ohiboka.com/admin/manage_bracelets .'
 		for b in nowe_bracelety:
 			msg_content += u'\r\n- '+b.name+ u', dodany '+str(b.date)+ u': http://patterns.ohiboka.com/bracelet/'+b.url
 		nowi_juzerzy = User.objects.filter(date_joined__gte = datetime.date.today() - datetime.timedelta(days = 7))
-		msg_content += u'\r\n\r\nZarejestrowało się '+str(len(nowi_juzerzy))+u' nowych użytkowników:'
+		msg_content += u'\r\n\r\nW ciągu ostatniego tygodnia zarejestrowało się '+str(len(nowi_juzerzy))+u' nowych użytkowników:'
 		for u in nowi_juzerzy:
 			msg_content += u'\r\n- '+u.username+ u', zarejestrowany '+str(u.date_joined)+ u': http://patterns.ohiboka.com/user/'+u.username
 		msg_content += u'\r\n\r\nhttp://ohiboka.com'
