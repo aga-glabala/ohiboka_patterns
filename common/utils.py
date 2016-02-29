decdcce7305ac293bb888dcf6ce5d5e249f72875
User Jakchcesz <jakchcesz@s0.mydevil.net>
2016-02-29 20:21:36 +0100

    facebook fix

--- common/utils.py
+++ common/utils.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from common.models import UserProfile
 from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
 import hashlib
 from datetime import date
+import logging
 
 
 class FacebookBackend(ModelBackend):
@@ -12,17 +14,20 @@ class FacebookBackend(ModelBackend):
 
     supports_inactive_user = False
 
-    def authenticate(self, fb_user=None):
+    def authenticate(self, fb_user = None):
+        logger = logging.getLogger(__name__)
         try:
-            user_profile = UserProfile.objects.get(fb_username = fb_user.username)
-            user_profile.user.backend = 'common.utils.FacebookBackend'
-            return user_profile.user
+            user_profile = UserProfile.objects.get(fb_username = fb_user.id)
+            UserModel = get_user_model()
+            user = UserModel._default_manager.get_by_natural_key(user_profile.user.username)
+            user.backend = 'registration.utils.FacebookBackend'
+            return user
         except UserProfile.DoesNotExist:
             # Create a new user. Note that we can set password
             # to anything, because it won't be checked; the password
             # from settings.py will.
-            username = fb_user.username
-            users = User.objects.filter(username__startswith=username + '-').order_by('id').reverse()
+            username = fb_user.id
+            users = User.objects.filter(username__startswith = username + '-').order_by('id').reverse()
             if not users:
                 users = User.objects.filter(username=username).order_by('id').reverse()
                 if users:
@@ -35,7 +40,7 @@ class FacebookBackend(ModelBackend):
             user.is_staff = False
             user.is_superuser = False
             user.save()
-            userprofile = UserProfile(fb_username=fb_user.username, fb_name=fb_user.name, user=user)
+            userprofile = UserProfile(fb_username = fb_user.id, fb_name = fb_user.name, user = user)
             userprofile.save()
             user.backend = 'common.utils.FacebookBackend'
             return user
